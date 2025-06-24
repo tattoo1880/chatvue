8fbff3875b9fd7205b13fa81d14969b4a6fe25cc
tattoo1880 <tattoo186225345941@gmail.com>
2025-06-24 19:43:27 +0800

    Update

--- src/utils/BaseUrl.js
+++ src/utils/BaseUrl.js
@@ -1,4 +1,4 @@
-// const APIURL =  "http://127.0.0.1:8080"
-const APIURL =  "http://80.66.196.161"
+const APIURL =  "http://127.0.0.1:8080"
+// const APIURL =  "http://80.66.196.161"
 
 export default APIURL;
