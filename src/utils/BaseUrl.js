4b52b8e9ec084f2845c61601a4922e6911d31ac0
tattoo1880 <tattoo186225345941@gmail.com>
2025-07-10 19:52:17 +0800

    Update

--- src/utils/BaseUrl.js
+++ src/utils/BaseUrl.js
@@ -1,4 +1,5 @@
 // const APIURL =  "http://127.0.0.1:8080"
-const APIURL =  "http://80.66.196.161"
+// const APIURL =  "http://80.66.196.161"
+const APIURL = "https://newtest.purecode.dpdns.org"
 
 export default APIURL;
